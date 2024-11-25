a32488637a493147bffd1d36f5bdc0d26e260ce5
mcvitiesdeez <ott_94@hotmail.com>
2024-11-26 06:22:44 +0800

    Implemented TransactionHistoryScreen with demo data

--- screens/TransactionHistoryScreen.tsx
+++ screens/TransactionHistoryScreen.tsx
@@ -1,19 +1,80 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Pressable } from 'react-native';
+import transactionData from '../data/transactionData.json';
 
 function TransactionHistoryScreen() {
+
+    const handleTransactionPress = () => {
+        console.log('Transaction Pressed');
+    }
+
     return (
-        <View style={styles.container}>
-            <Text>Transaction History Screen</Text>
+        <View style={styles.appContainer}>
+            <View style={styles.titleContainer}>
+                <Text style={styles.titleText}>Transaction History</Text>
+            </View>
+            <View style={styles.transactionContainer}>
+                <FlatList
+                    data={transactionData}
+                    keyExtractor={(item) => item.transactionID}
+                    renderItem={({ item }) => (
+                        <View style={styles.transactionItem}>
+                            <Pressable onPress={handleTransactionPress} android_ripple={{ color: '#cccccc' }}>
+                                <Text style={styles.transactionText}>{item.date}</Text>
+                                <Text style={styles.transactionText}>{item.description}</Text>
+                                <View style={styles.rowContainer}>
+                                    <Text style={styles.transactionText}>{item.type}</Text>
+                                    <Text style={styles.transactionText}>Amount: RM{item.amount.toFixed(2)}</Text>
+                                </View>
+                            </Pressable>
+                        </View>
+                    )}
+                />
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
+    appContainer: {
+        padding: 50,
+        paddingHorizontal: 16,
+        flex: 1
+    },
+    titleContainer: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
         justifyContent: 'center',
+        borderBottomWidth: 1,
+        borderBottomColor: '#cccccc',
+        marginBottom: 10,
+    },
+    titleText: {
+        fontSize: 34,
+        fontWeight: 'bold',
+        color: 'black',
+    },
+    transactionContainer: {
+        flex: 8,
+    },
+    transactionItem: {
+        margin: 8,
+        borderRadius: 6,
+        backgroundColor: '#5e0acc',
+        fontWeight: 'bold',
+    },
+    transactionText: {
+        color: 'white',
+        paddingVertical: 4,
+        paddingHorizontal: 8,
+        fontWeight: 'bold',
+    },
+    rowContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    masked: {
+        color: '#888',
+        fontStyle: 'italic',
     },
 });
 
